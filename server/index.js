326c73b7e4825938866e47e481a79789953e91f7
Hiebeler <emanuel.hiebeler@gmail.com>
2022-05-19 15:29:56 +0200

    works with nodejs

---
+++ server/index.js
@@ -0,0 +1,14 @@
+var express = require('express')
+const path = require('path');
+
+var app = express();
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+app.listen(3000, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`)
+})
