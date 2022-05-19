2f84687e1de525f87777cd5cff689853e984f608
Hiebeler <emanuel.hiebeler@gmail.com>
2022-05-19 15:52:32 +0200

    added everything to github

---
+++ client/src/Components/Card.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Box } from '@mui/material';
+
+const Card = props => {
+    const {image, clicked, open, cardSet} = props;
+    let imageLink = "";
+    if (open) {
+        imageLink = require("../images/set" + cardSet + "/" + image);
+    } else {
+        imageLink = require("../images/back.png");
+    }
+
+    return (
+        <div>
+            <Box
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+            >
+                <img src={imageLink} alt="not found" onClick={clicked} width="200px" height="200px"/>
+            </Box>
+        </div>
+    );
+};
+
+export default Card;
---
+++ client/src/Components/styles/navbar.styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const SettingsDiv = styled.div`
+  margin-left: 60px;
+`
+
+export const AligntRightDiv = styled.div`
+  margin-left: auto;
+  margin-right: 0;
+`
