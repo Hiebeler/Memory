2ad8553daca8cf629196c35167955731a0f9eb4e
Hiebeler <emanuel.hiebeler@gmail.com>
2022-06-02 14:21:52 +0200

    removed react from backend

--- server/index.js
+++ server/index.js
@@ -3,16 +3,10 @@ const path = require('path');
 
 var app = express();
 
-app.use(express.static(path.resolve(__dirname, '../client/build')));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
-
 app.get('/api/getScoreboard', (req, res) => {
     res.send("moin");
 });
 
 app.listen(8080, () => {
-    console.log(`Example app listening on port ${process.env.PORT}`)
+    console.log(`Example app listening on port  8080`)
 })
