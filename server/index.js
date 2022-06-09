5617141d114e5d64a8097e7016c07a2dd5b1482a
botrick01 <johannsenpatrick.privat@gmail.com>
2022-06-09 14:10:24 +0200

    post added

--- server/index.js
+++ server/index.js
@@ -2,9 +2,31 @@ var express = require('express')
 const path = require('path');
 
 var app = express();
+app.use(express.json())
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://mongoadmin:mypasswd@10.115.3.24:8017/";
+
+
 
 app.get('/api/getScoreboard', (req, res) => {
     res.send("moin");
+});
+
+app.post('/api/postScore', (req, res) => {
+    let name = req.body.name;
+    let score = req.body.score;
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("memory");
+        var myobj = {'name': name, 'score': score};
+        dbo.collection("scoreBoard").insertOne(myobj, function (err, _res) {
+            if (err) throw err;
+            console.log("1 document inserted");
+            db.close();
+            res.send("worked");
+        });
+    });
 });
 
 app.listen(8080, () => {
