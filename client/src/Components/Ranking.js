da60415d515baa567c0f12267c64e8d2a8baa543
Hiebeler <emanuel.hiebeler@gmail.com>
2022-06-23 14:14:55 +0200

    ranking output works

---
+++ client/src/Components/Ranking.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class Rankings extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            ranking: []
+        }
+    }
+
+    async componentDidMount() {
+        const fetchUserEmail = async () => {
+            return fetch("http://localhost:8080/api/getScoreboard").then(async (data) => {
+                return await data.json()
+            });
+        };
+        this.setState({
+            ranking: await fetchUserEmail()
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.ranking.map((rank) => {
+                    console.log(rank);
+                    return <div>{rank.name + "  " + rank.score}</div>
+                })}
+            </div>
+        );
+    }
+}
+
+
+
+export default Rankings;
